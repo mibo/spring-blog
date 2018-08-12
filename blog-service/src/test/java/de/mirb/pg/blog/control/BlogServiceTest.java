a948e275a328d2d1e0e83b6278e92c623a437734
mibo <mibo@mirb.de>
2018-08-12 19:09:10 +0200

    Fixed test compile issue

--- blog-service/src/test/java/de/mirb/pg/blog/control/BlogServiceTest.java
+++ blog-service/src/test/java/de/mirb/pg/blog/control/BlogServiceTest.java
@@ -1,7 +1,13 @@
 package de.mirb.pg.blog.control;
 
 import de.mirb.pg.blog.entity.BlogEntry;
+import de.mirb.pg.blog.entity.BlogRepository;
 import org.junit.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -10,8 +16,13 @@ import static org.junit.Assert.assertEquals;
 public class BlogServiceTest {
 
   @Test
-  public void createEntry() {
-    BlogService service = new BlogService();
+  public void createAndGetEntry() {
+    BlogRepository mockedRepo = Mockito.mock(BlogRepository.class);
+    Mockito.when(mockedRepo.save(Mockito.any(BlogEntry.class))).thenReturn(
+        Mono.just(BlogEntry.createBy("mibo").content("some content").create()));
+    Mockito.when(mockedRepo.findAll()).thenReturn(
+        Flux.just(BlogEntry.createBy("mibo").content("some content").create()));
+    BlogService service = new BlogService(mockedRepo);
     //
     service.createEntry("mibo", "some content");
     //
@@ -21,13 +32,33 @@ public class BlogServiceTest {
     assertThat(entry.getContent(), is("some content"));
   }
 
+  @Test
+  public void createEntry() {
+    BlogRepository mockedRepo = Mockito.mock(BlogRepository.class);
+    BlogEntry e = BlogEntry.createBy("mibo").content("some content").create();
+    Mockito.when(mockedRepo.save(Mockito.refEq(e, "id", "createdAt"))).thenReturn(Mono.just(e));
+    BlogService service = new BlogService(mockedRepo);
+    //
+    BlogEntry created = service.createEntry("mibo", "some content");
+    assertThat(created.getCreatedBy(), is("mibo"));
+    assertThat(created.getContent(), is("some content"));
+  }
+
 
   @Test
   public void updateEntry() {
-    BlogService service = new BlogService();
+    BlogRepository mockedRepo = Mockito.mock(BlogRepository.class);
+    BlogEntry created = BlogEntry.createBy("mibo").content("some content").create();
+    BlogEntry updated = BlogEntry.createBy("mibo").content("another content").create();
+    Mockito.when(mockedRepo.save(Mockito.refEq(created, "id", "createdAt"))).thenReturn(Mono.just(created));
+    Mockito.when(mockedRepo.save(Mockito.refEq(updated, "id", "createdAt"))).thenReturn(Mono.just(updated));
+    Mockito.when(mockedRepo.findAll()).thenReturn(Flux.just(updated));
+    Mockito.when(mockedRepo.findById(created.getId())).thenReturn(Mono.just(updated));
+
+    BlogService service = new BlogService(mockedRepo);
     //
-    BlogEntry created = service.createEntry("mibo", "some content");
-    service.updateEntry(created.getId(), "another content");
+    BlogEntry c = service.createEntry("mibo", "some content");
+    service.updateEntry(c.getId(), "another content");
 
     //
     assertEquals(1, service.getAllEntries().size());
