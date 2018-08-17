968e9da87a43b58e7537a759c41a0bce3759af26
mibo <mibo@mirb.de>
2018-08-17 22:29:32 +0200

    Started with ITest project

---
+++ blog-service-it/src/test/java/de/mirb/pg/blog/it/BasicTest.java
@@ -0,0 +1,13 @@
+package de.mirb.pg.blog.it;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+
+  @Test
+  public void testBasics() {
+    assertTrue(true);
+  }
+}
